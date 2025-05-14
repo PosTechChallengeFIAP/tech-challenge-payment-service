8be5c65e28e8ae305461c4a6f1a80dd4611d7b6a
GregoriSimei <gregorisimei@gmail.com>
2025-05-13 23:08:55 -0300

    chore: initial structure

---
+++ .docker/db/create-schema.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Define schema default se não existir
+SCHEMA="${POSTGRES_SCHEMA:-tech-challenge-payment-service}"
+
+echo "Creating schema '$SCHEMA'..."
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE SCHEMA IF NOT EXISTS "$SCHEMA";
+EOSQL
+
+echo "Schema '$SCHEMA' created successfully."
